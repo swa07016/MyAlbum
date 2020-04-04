51307ea9335c8851c8ca47624b94586ea353e3d9
swa07016 <swa07016@naver.com>
2020-04-04 22:40:05 +0900

    Create Card Model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    title = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='images/')
+    description = models.CharField(max_length=500)
+    writer = models.CharField(max_length=255)
+    date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
